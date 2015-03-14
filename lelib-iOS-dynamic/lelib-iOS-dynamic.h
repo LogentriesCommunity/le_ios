d552ef64e39737126935b417a1b963a79bd4d453
Alexander Dodatko <dodikk88.reg@gmail.com>
2015-03-14 15:29:38 +0200

    Added dynamic framework target

---
+++ lelib-iOS-dynamic/lelib-iOS-dynamic.h
@@ -0,0 +1,19 @@
+//
+//  lelib-iOS-dynamic.h
+//  lelib-iOS-dynamic
+//
+//  Created by dodikk on 3/14/15.
+//  Copyright (c) 2015 Logentries. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for lelib-iOS-dynamic.
+FOUNDATION_EXPORT double lelib_iOS_dynamicVersionNumber;
+
+//! Project version string for lelib-iOS-dynamic.
+FOUNDATION_EXPORT const unsigned char lelib_iOS_dynamicVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <lelib_iOS_dynamic/PublicHeader.h>
+
+
